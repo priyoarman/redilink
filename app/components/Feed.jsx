e0c2990d3bd11a3d5cf7b0a5c87e8beb10c7e47b
Arman Hossain <priyoarman1996@gmail.com>
2025-12-01 00:06:41 +0100

    Make changes to mobile UI

--- app/components/Feed.jsx
+++ app/components/Feed.jsx
@@ -9,12 +9,10 @@ export default function Feed() {
   return (
     <div className="sticky z-10 container flex flex-col border-r-1 border-gray-200 py-2 lg:w-2/4">
       <div className="flex h-12 flex-row items-stretch justify-between border-b-1 border-gray-200 sm:hidden">
-        <div className="flex cursor-pointer px-4">
-          <MiniProfile />
-        </div>
+          <div className="flex h-10 w-10 rounded-full bg-neutral-600 ml-2 cursor-pointer border-1 border-gray-200 px-3 pt-1"></div>
         <Link
           href={"/"}
-          className="text-blue-40 flex w-fit items-center justify-center space-x-2 rounded-3xl px-4 pr-10 pb-2 text-[28px] font-bold text-blue-400 transition duration-200 hover:bg-gray-200 lg:w-fit lg:px-4"
+          className="text-blue-40 flex w-fit items-center justify-center space-x-2 rounded-3xl px-4 pb-2 mx-auto text-[28px] font-bold text-blue-400 transition duration-200 hover:bg-gray-200 lg:w-fit lg:px-4"
         >
           <Image
             src="/ReDI.png"
